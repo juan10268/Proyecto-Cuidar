9e419e9068fe3e1ecb9a8b507a9798b74d1756e4
Cristian David Rivera Florez <crisdavid1987@hotmail.com>
2019-03-14 18:07:29 -0500

    mejoras en la vista agregar persona por Cristhian 14/03/19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './login/login.component';
 import { AgregarEspecialistaComponent } from './especialista/agregar-especialista/agregar-especialista.component';
 import { ActualizarEspecialistaComponent } from './especialista/actualizar-especialista/actualizar-especialista.component';
 import { ListarEspecialistaComponent } from './especialista/listar-especialista/listar-especialista.component';
+import { ListarPersonaComponent } from './persona/listar-persona/listar-persona.component';
+import { ListarPacienteComponent } from './paciente/listar-paciente/listar-paciente.component';
+import { AgregarPersonaComponent } from './persona/agregar-persona/agregar-persona.component';
+import { ActualizarPersonaComponent } from './persona/actualizar-persona/actualizar-persona.component';
 
 
 const routes: Routes = [
@@ -16,7 +20,11 @@ const routes: Routes = [
   { path:'agregar-paciente', component: AgregarPacienteComponent},
   { path:'agregar-especialista', component: AgregarEspecialistaComponent},
   { path:'actualizar-especialista', component: ActualizarEspecialistaComponent},
-  { path:'listar-especialista', component: ListarEspecialistaComponent} 
+  { path:'listar-especialista', component: ListarEspecialistaComponent},
+  { path:'listar-persona', component: ListarPersonaComponent },
+  { path:'listar-paciente', component: ListarPacienteComponent },
+  { path:'agregar-persona', component: AgregarPersonaComponent },
+  { path:'actualizar-persona', component: ActualizarPersonaComponent },
   
 ];
 
